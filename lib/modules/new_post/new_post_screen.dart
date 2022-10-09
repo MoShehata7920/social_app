8e0175d725fcbb257288530babf71244c43af29b
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2022-10-09 21:41:24 +0200

    designed of the add new post screen

--- lib/modules/new_post/new_post_screen.dart
+++ lib/modules/new_post/new_post_screen.dart
@@ -2,24 +2,98 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/layout/cubit/cubit.dart';
+import 'package:social_app/layout/cubit/states.dart';
+import 'package:social_app/shared/component/component.dart';
 import 'package:social_app/shared/styles/icon_broken.dart';
+
+import '../../shared/styles/colors.dart';
 
 class NewPostScreen extends StatelessWidget {
   const NewPostScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // iconTheme: IconBroken.Arrow___Left_2,
-        leading: IconButton(
-          icon: Icon(IconBroken.Arrow___Left_2),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: const Text('Add Post'),
-      ),
+    return BlocConsumer<SocialCubit, SocialStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            // iconTheme: IconBroken.Arrow___Left_2,
+            leading: IconButton(
+              icon: const Icon(IconBroken.Arrow___Left_2),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: const Text('Create Post'),
+            actions: [
+              defaultTextButton(function: () {}, text: 'Post'),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const CircleAvatar(
+                      radius: 25,
+                      backgroundImage: NetworkImage(
+                        'https://th.bing.com/th/id/OIP.g-AmzU4RIkeTm904gJ3tjgHaFj?pid=ImgDet&rs=1',
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    // ignore: prefer_const_constructors
+                    Expanded(
+                      child: const Text(
+                        'Mohamed Shehata',
+                        style: TextStyle(
+                          height: 1.4,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'What is on your mind .... ',
+                        border: InputBorder.none),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            // ignore: prefer_const_literals_to_create_immutables
+                            children: [
+                              const Icon(IconBroken.Image),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              const Text('Add Photo'),
+                            ],
+                          )),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                          onPressed: () {}, child: const Text('# Add')),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
